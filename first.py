cc3de946b29cd2daf70fbdaf3aac81c870a8b4ec
cutepanda62 <nishasharma62022@gmail.com>
2026-06-22 11:51:16 +0530

    first commit

--- first.py
+++ first.py
@@ -1,2 +1,14 @@
-print("This is from python ")
-print("This is from local python ")
+#1.waf to remove last word from  string
+#2.waf to remove words if vawel present from string
+#3.waf to 
+str1="This is python code in vs"
+#4.wof to extract only numbers for string
+str1="This is python code in vs version-35 address noida-11096"
+
+
+text="This is python code in vs"
+def remove_last_word(s):
+    words = s.split()
+    return " ".join(words[:-1])
+
+print(remove_last_word(text))
